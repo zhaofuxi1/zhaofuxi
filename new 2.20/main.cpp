fe59ad632886095ba32d3e51bd89d6713a57b82f
zhaofuxi11 <2014920717@qq.com>
2019-03-09 17:12:25 +0800

    测绘四班赵福玺作业

---
+++ new 2.20/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r = 0;
+    int D = 0;
+    int C = 0;
+    int S = 0;
+    cout<<"Enter a integer as r";
+    cin>>r;
+    D=2*r;
+    C=2*3,14159*r;
+    S=3.14159*r*r;
+    cout<<"D="<<D<<"\nC="<<C<<"\nS="<<S<<endl;
+    return 0;
+}
---
+++ new216/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int he = 0;
+    int cha = 0;
+    int ji = 0;
+    int shang = 0;
+    cout<<"Enter two integer";
+    cin>>a>>b;
+    he=a+b;
+    cha=a-b;
+    ji=a*b;
+    shang=a/b;
+    cout<<"he"<<he<<"cha"<<cha<<"ji"<<ji<<"shang"<<endl;
+
+
+    return 0;
+}
